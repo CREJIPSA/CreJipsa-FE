918948b63737f3206e480e3fe66d38a6decd1edd
yeseul <joyri0202@gmail.com>
2025-11-08 00:12:29 +0900

    feat(landing): landing 화면 UI 레이아웃 구현

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  const router = useRouter();
+  const isLoggedIn = false; // Replace with actual authentication logic
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace("/landing");
+    }
+  }, [isLoggedIn, router]);
+
   return (
     <View
       style={{
---
+++ app/landing.js
@@ -0,0 +1,88 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const Landing = () => {
+
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.landingContainer, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
+      <View style={styles.logoContainer}>
+        <Text>logo&graphic</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.mainText}>크집사에 오신걸 환영합니다.</Text>
+        <Text style={styles.subText}>크리에이터를 위한 어시스턴트 앱</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </Pressable>
+        <Pressable style={styles.signUpButton}>
+          <Text style={styles.signUpButtonText}>회원가입</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
+
+export default Landing;
+
+const styles = StyleSheet.create({
+  landingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d9d9d9',
+  },
+  logoContainer: {
+    flex: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  subText: {
+    fontSize: 16,
+    fontWeight: 'medium',
+  },
+  buttonContainer: {
+    flex: 2,
+    alignItems: 'center',
+    paddingTop: 15,
+  },
+  loginButton: {
+    width: 250,
+    height: 42,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 100,
+    marginBottom: 8,
+  }, 
+  signUpButton: {
+    width: 250,
+    height: 42,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 100,
+  }, 
+  loginButtonText: {
+    color: '#000000',
+    fontSize: 16,   
+    fontWeight: 'bold',
+  },
+  signUpButtonText: {
+    color: '#000000',
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+});
