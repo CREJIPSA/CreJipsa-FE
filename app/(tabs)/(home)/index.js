60bd4ead29cdf6d58a25ffdd7a9b2c812b4f44a7
yeseul <joyri0202@gmail.com>
2025-11-27 02:16:22 +0900

    feat(main-home): #12 - 트렌드 리포트 UI 레이아웃 구현 & 실시간 트렌드 SectionList를 FlatList로 변경

--- app/(tabs)/(home)/index.js
+++ app/(tabs)/(home)/index.js
@@ -1,59 +1,81 @@
-import { format } from 'date-fns';
-import { Dimensions, SectionList, Text, View, useColorScheme } from "react-native";
-import TrendCard, { getFirstTrendCardPath, getFourthTrendCardPath, getSecondTrendCardPath, getThirdTrendCardPath } from '../../../components/trendcard';
+import { format } from "date-fns";
+import {
+  Dimensions,
+  ScrollView,
+  Text,
+  View,
+  useColorScheme,
+} from "react-native";
+
+import TrendCard, {
+  getFirstTrendCardPath,
+  getFourthTrendCardPath,
+  getSecondTrendCardPath,
+  getThirdTrendCardPath,
+} from "../../../components/trendcard";
 
 export default function Home() {
-
   const colorScheme = useColorScheme();
-  const isDark = colorScheme === 'dark';
+  const isDark = colorScheme === "dark";
   const styles = getStyles(isDark);
 
-  const realTimeTrendData = [ // 임의
-      {
-        rank: 1,
-        tag: "#음악",
-        title: "AOA 짧은 치마",
-        width: Dimensions.get('window').width * 0.75,
-        height: Dimensions.get('window').height * 0.43,
-        path: getFirstTrendCardPath(Dimensions.get('window').width * 0.75, Dimensions.get('window').height * 0.43),
-        fill: "#CCFF66"
-      },
-      {
-        rank: 4,
-        tag: "#음악",
-        title: "00 챌린지",
-        width: Dimensions.get('window').width * 0.29,
-        height: Dimensions.get('window').height * 0.22,
-        path: getFourthTrendCardPath(Dimensions.get('window').width * 0.29, Dimensions.get('window').height * 0.22),
-        fill: "#ED7658"
-      },
-      {
-        rank: 2,
-        tag: "#게임",
-        title: "배틀 그라운드",
-        width: Dimensions.get('window').width * 0.60,
-        height: Dimensions.get('window').height * 0.2, 
-        path: getSecondTrendCardPath(Dimensions.get('window').width * 0.60, Dimensions.get('window').height * 0.2),
-        fill: "#4B4AEE"
-      }, 
-      {
-        rank: 3,
-        tag: "#게임",
-        title: "배틀 그라운드",
-        width: Dimensions.get('window').width * 0.44,
-        height: Dimensions.get('window').height * 0.2,
-        path: getThirdTrendCardPath(Dimensions.get('window').width * 0.44, Dimensions.get('window').height * 0.2),
-        fill: "#E1FFA4"
-      },
+  // (임시) 현재 시간을 마지막 업데이트로 설정
+  const lastUpdateTime = format(new Date(), "yyyy년 MM월 dd일 HH:mm");
+
+  {
+    /* 실시간 트렌드 */
+  }
+  const realTimeTrendData = [
+    // 임의
+    {
+      rank: 1,
+      tag: "#음악",
+      title: "AOA 짧은 치마",
+      width: Dimensions.get("window").width * 0.75,
+      height: Dimensions.get("window").height * 0.43,
+      path: getFirstTrendCardPath(
+        Dimensions.get("window").width * 0.75,
+        Dimensions.get("window").height * 0.43
+      ),
+      fill: "#CCFF66",
+    },
+    {
+      rank: 4,
+      tag: "#음악",
+      title: "샤넬 챌린지",
+      width: Dimensions.get("window").width * 0.29,
+      height: Dimensions.get("window").height * 0.22,
+      path: getFourthTrendCardPath(
+        Dimensions.get("window").width * 0.29,
+        Dimensions.get("window").height * 0.22
+      ),
+      fill: "#ED7658",
+    },
+    {
+      rank: 2,
+      tag: "#게임",
+      title: "배틀 그라운드",
+      width: Dimensions.get("window").width * 0.6,
+      height: Dimensions.get("window").height * 0.2,
+      path: getSecondTrendCardPath(
+        Dimensions.get("window").width * 0.6,
+        Dimensions.get("window").height * 0.2
+      ),
+      fill: "#4B4AEE",
+    },
+    {
+      rank: 3,
+      tag: "#게임",
+      title: "배틀 그라운드",
+      width: Dimensions.get("window").width * 0.44,
+      height: Dimensions.get("window").height * 0.2,
+      path: getThirdTrendCardPath(
+        Dimensions.get("window").width * 0.44,
+        Dimensions.get("window").height * 0.2
+      ),
+      fill: "#E1FFA4",
+    },
   ];
-  const rawData = [
-    { title: "실시간 트렌드", data: [ null ] },
-    { title: "트렌드 리포트", data: ["Item 4", "Item 5", "Item 6"] },
-  ]
-  const DATA = rawData.map(section => ({
-    title: section.title,
-    data: section.data,
-  }));
 
   const renderCard = (c) => {
     return (
@@ -65,11 +87,15 @@ export default function Home() {
         fill={c.fill}
         contentStyle={c.contentStyle}
       >
-        <View style={{ flex: 1, position: 'relative', height: c.height }}>  
-          <View style={c.rank === 3 && {
-            position: 'absolute',
-            left: c.width * 0.33
-          }}>
+        <View style={{ flex: 1, position: "relative", height: c.height }}>
+          <View
+            style={
+              c.rank === 3 && {
+                position: "absolute",
+                left: c.width * 0.33,
+              }
+            }
+          >
             <Text style={styles.rankText}>{c.rank}위</Text>
             {c.tag && (
               <View style={styles.tag}>
@@ -77,131 +103,265 @@ export default function Home() {
               </View>
             )}
           </View>
-          <View style={{ position: 'absolute', bottom: 20, left: 20, width: c.width - 30 }}>
-            <Text 
+          <View
+            style={{
+              position: "absolute",
+              bottom: 20,
+              left: 20,
+              width: c.width - 30,
+            }}
+          >
+            <Text
               style={styles.trendCardTitleText}
               numberOfLines={2}
-              ellipsizeMode='tail'
-            >{c.title}</Text>
+              ellipsizeMode="tail"
+            >
+              {c.title}
+            </Text>
           </View>
         </View>
       </TrendCard>
     );
   };
 
-  // (임시) 현재 시간을 마지막 업데이트로 설정
-  const lastUpdateTime = format(new Date(), 'yyyy년 MM월 dd일 HH:mm');
+  {
+    /* 트렌드 리포트 */
+  }
+  const fieldData = [
+    {
+      field: "전체",
+      data: [
+        {
+          title: "아이폰 16 pro",
+          tag: "일상/밈",
+        },
+        {
+          title: "듀 가나디",
+          tag: "일상/밈",
+        },
+        {
+          title: "쉐이칸샹",
+          tag: "일상/밈",
+        },
+        {
+          title: "퉁퉁퉁퉁퉁퉁퉁퉁퉁 사후르",
+          tag: "일상/밈",
+        },
+      ],
+    },
+    {
+      field: "게임",
+      data: [
+        {
+          title: "배틀그라운드",
+          tag: "게임",
+        },
+        {
+          title: "오버워치",
+          tag: "게임",
+        },
+        {
+          title: "롤",
+          tag: "게임",
+        },
+      ],
+    },
+    {
+      field: "뷰티",
+      data: [
+        {
+          title: "올영 블프",
+          tag: "뷰티",
+        },
+        {
+          title: "롬앤",
+          tag: "뷰티",
+        },
+        {
+          title: "다이소 뷰티템",
+          tag: "뷰티",
+        },
+      ],
+    },
+  ];
 
   return (
-    <View style={styles.mainContainer}>
-      <SectionList 
-        sections={DATA}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({item, section}) => {
-          if (section.title === "실시간 트렌드") {
-            return (
-              <View style={{ justifyContent: 'flex-end' }}>
-                <View style={styles.trendCardTopContainer}>
-                  {renderCard(realTimeTrendData[0])}
-                  <View style={styles.rank4TrendCard}>
-                    {renderCard(realTimeTrendData[1])}
-                  </View>
-                </View>
-                <View style={styles.trendCardBottomContainer}>
-                  {renderCard(realTimeTrendData[2])}
-                  <View style={styles.rank3TrendCard}>
-                    {renderCard(realTimeTrendData[3])}
+    <ScrollView style={styles.mainContainer}>
+      <View style={styles.sectionHeader}>
+        <Text style={styles.titleText}>실시간 트렌드</Text>
+        <Text style={styles.lastUpdateText}>
+          마지막 업데이트{"\n"}
+          {lastUpdateTime}
+        </Text>
+      </View>
+      <View style={{ justifyContent: "flex-end" }}>
+        <View style={styles.trendCardTopContainer}>
+          {renderCard(realTimeTrendData[0])}
+          <View style={styles.rank4TrendCard}>
+            {renderCard(realTimeTrendData[1])}
+          </View>
+        </View>
+        <View style={styles.trendCardBottomContainer}>
+          {renderCard(realTimeTrendData[2])}
+          <View style={styles.rank3TrendCard}>
+            {renderCard(realTimeTrendData[3])}
+          </View>
+        </View>
+      </View>
+      <View style={styles.sectionHeader}>
+        <Text style={styles.titleText}>트렌드 리포트</Text>
+      </View>
+      {fieldData.map((fieldData) => (
+        <View key={fieldData.field}>
+          <View style={styles.fieldSectionHeader}>
+            <Text style={styles.fieldText}>
+              이번주{" "}
+              <Text style={{ fontWeight: "bold" }}>{fieldData.field}</Text>{" "}
+              분야의 트렌드 추천
+            </Text>
+          </View>
+          <View style={styles.trendReportCardContainer}>
+            {fieldData.data.map((data, index) => (
+              <View key={index}>
+                <View style={styles.trendReportCard}>
+                  <View style={styles.trendReportIcon} />
+                  <View style={styles.trendReportContents}>
+                    <Text style={styles.trendReportTitleText}>
+                      {data.title}
+                    </Text>
+                    <View style={styles.trendReportTag}>
+                      <Text style={styles.trendReportTagText}>#{data.tag}</Text>
+                    </View>
                   </View>
                 </View>
               </View>
-            )
-          } else { // 트렌드 리포트
-            return (
-              <Text>{item}</Text>
-            )
-          }
-        }}
-        renderSectionHeader={({section: {title}}) => (
-          <View style={styles.header}>
-            <Text style={styles.titleText}>{title}</Text>
-            {title === "실시간 트렌드" && (
-              <Text style={styles.lastUpdateText}>마지막 업데이트{'\n'}{lastUpdateTime}</Text>
-            )}
+            ))}
           </View>
-        )}
-      />
-    </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
 const getStyles = (isDark) => ({
   mainContainer: {
     flex: 1,
-    backgroundColor: isDark ? '#202020' : '#fff',
+    backgroundColor: isDark ? "#202020" : "#fff",
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  sectionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 16,
     paddingTop: 30,
     marginBottom: 20,
   },
   titleText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: isDark ? '#FAFAFA' : '#000',
+    fontWeight: "bold",
+    color: isDark ? "#FAFAFA" : "#000",
   },
   lastUpdateText: {
     fontSize: 12,
-    color: isDark ? '#D3D3D3' : '#555',
-    textAlign: 'right',
+    color: isDark ? "#D3D3D3" : "#555",
+    textAlign: "right",
   },
   trendCardTopContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
+    flexDirection: "row",
+    alignItems: "flex-end",
     paddingHorizontal: 16,
-    position: 'relative',
+    position: "relative",
   },
   rank4TrendCard: {
-    position: 'absolute',
+    position: "absolute",
     right: 16,
     borderRadius: 30,
   },
   trendCardBottomContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
+    flexDirection: "row",
+    alignItems: "flex-end",
     paddingHorizontal: 16,
     marginTop: 15,
-    position: 'relative',
+    marginBottom: 40,
+    position: "relative",
   },
   rank3TrendCard: {
-    position: 'absolute',
+    position: "absolute",
     right: 16,
   },
   rankText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: isDark ? '#141414' : '#FFF',
+    fontWeight: "bold",
+    color: isDark ? "#141414" : "#FFF",
     paddingTop: 20,
     paddingLeft: 25,
   },
   tag: {
     marginTop: 8,
     marginLeft: 20,
-    backgroundColor: '#141414',
+    backgroundColor: "#141414",
     padding: 8,
     borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   tagText: {
     fontSize: 16,
-    color: '#FAFAFA',
+    color: "#FAFAFA",
   },
   trendCardTitleText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#141414',
+    fontWeight: "bold",
+    color: "#141414",
+  },
+  fieldSectionHeader: {
+    paddingHorizontal: 16,
+    paddingTop: 20,
+  },
+  fieldText: {
+    fontSize: 20,
+    color: "#FAFAFA",
+  },
+  trendReportCardContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    paddingHorizontal: 16,
+    marginTop: 20,
+  },
+  trendReportCard: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: isDark ? "#FAFAFA" : "#F5F5F5",
+    borderRadius: 10,
+    paddingVertical: 14,
+    paddingHorizontal: 12,
+    gap: 16,
+    marginRight: 8,
+    marginBottom: 12,
+  },
+  trendReportIcon: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#000000",
+    borderRadius: 8,
+  },
+  trendReportContents: {
+    gap: 6,
+    alignItems: "flex-start",
+  },
+  trendReportTitleText: {
+    fontSize: 18,
+    color: isDark ? "#141414" : "#000",
+  },
+  trendReportTag: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: isDark ? "#202020" : "#D3D3D3",
+    borderRadius: 20,
+  },
+  trendReportTagText: {
+    fontSize: 14,
+    color: isDark ? "#FAFAFA" : "#777777",
   },
 });
