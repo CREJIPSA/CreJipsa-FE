d86c27d66146dc05d71db0748c94e518a1e92a04
suucong <suucong2@swu.ac.kr>
2025-12-05 02:30:32 +0900

    chore(asset): #20 - tiktok 로고 이미지 이름 수정

--- app/(tabs)/(home)/_layout.js
+++ app/(tabs)/(home)/_layout.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import instagramLogo from '../../../assets/images/instagram_logo.png';
-import tictokLogo from '../../../assets/images/tictok_logo.png';
+import tictokLogo from '../../../assets/images/tiktok_logo.png';
 import youtubeLogo from '../../../assets/images/youtube_logo.png';
 import AlarmIcon from '../../../assets/svgs/home-alarm-icon';
 import SearchIcon from '../../../assets/svgs/home-search-icon';
--- app/sign-up.js
+++ app/sign-up.js
@@ -12,7 +12,7 @@ import {
 } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import instagramLogo from '../assets/images/instagram_logo.png';
-import tictokLogo from '../assets/images/tictok_logo.png';
+import tictokLogo from '../assets/images/tiktok_logo.png';
 import youtubeLogo from '../assets/images/youtube_logo.png';
 import GenderFemaleIcon from '../assets/svgs/signup-gender-female-icon';
 import GenderMaleIcon from '../assets/svgs/signup-gender-male-icon';
